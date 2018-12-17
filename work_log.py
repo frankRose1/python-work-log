1381ce5cd8fde2da6f6294a5ef914d63b14a13d0
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-12-16 23:58:08 -0500

    Add basic logic/user menu

--- work_log.py
+++ work_log.py
@@ -1,8 +1,32 @@
 import csv
 
 
+def get_user_choice():
+    return input('Your choice: ')
+
+
+def print_menu():
+    print('Choose an option from the menu')
+    print('1 --> Do something')
+    print('2 --> Do something else')
+    print('Q --> Quit the program')
+
+
 def init():
-    print('init')
+    waiting_for_input = True
+    while waiting_for_input:
+        print_menu()
+        user_choice = get_user_choice()
+        if user_choice == '1':
+            print('doing something')
+        elif user_choice == '2':
+            print('doing something else')
+        elif user_choice.upper() == 'Q':
+            waiting_for_input = False
+        else:
+            print('Option not recognized, please try again.')
+
+    print('User has ended the program.')
 
 
 if __name__ == '__main__':
