287c75b42c4f1c25f060c9a8181ace00c96b271d
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-12-28 18:47:06 -0500

    Did some refactoring and improved how menus are printed

--- readme.md
+++ readme.md
@@ -1,12 +1,16 @@
 # Python Work Log
+
 Terminal application that allows a user to enter tasks which will then be saved as a row in a csv file.
-Date, title, minutes, and optional notes can all be added to the csv. You can also search by exact date or exact minutes and the task(s)
+Date, title, minutes, and optional notes can all be added to the csv. There are various search options and any matching task(s)
 will be printed to the console.
 
 ## App Features
-* Object oriented approach
-* ```LogUtil``` class will check for the ```log.csv``` when the app launches and create it if it doesn't exist
-* tasks are loaded from the file system when the app starts
-* Can search by:
-  * exact Date
-  * time spent
+
+- Object oriented approach
+- `LogUtil` class will check for the `log.csv` when the app launches and create it if it doesn't exist
+- tasks are loaded from the file system when the app starts
+- Can search by:
+  - exact Date
+  - time spent
+  - exact title or notes
+  - enter a regex pattern that will look for a match in either title or notes
--- work_log.py
+++ work_log.py
@@ -9,54 +9,62 @@ from collections import OrderedDict
 from log_util import LogUtil
 from search_util import SearchUtil
 
+MAIN_MENU = OrderedDict([
+    ('a', 'Add new entry'),
+    ('b', 'Search existing entries'),
+    ('c', 'Quit the program')
+])
+
+SEARCH_MENU = OrderedDict([
+    ('a', 'Exact Date'),
+    ('b', 'Time spent (in minutes)'),
+    ('c', 'Exact Search (will search title and notes)'),
+    ('d', 'Regex Pattern'),
+    ('e', 'Return to main menu')
+])
+
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 
 class WorkLog:
+    """ Provides the interface needed for a user to interact with the application """
     def __init__(self):
         self.tasks = LogUtil.load_tasks()
         self.prompt_user()
 
-    def clear_screen(self):
-        os.system('cls' if os.name == 'nt' else 'clear')
-
     def search_menu(self):
-        self.clear_screen()
-        waiting_for_input = True
-        while waiting_for_input:
+        clear_terminal()
+        while True:
             print('Do you want to search by:')
-            print('a) Exact Date')
-            print('b) Time spent(in minutes)')
-            print('c) Exact Search (will search title and notes)')
-            print('d) Range of dates')
-            print('e) Regex Pattern')
-            print('f) Return to main menu')
-            user_choice = self.get_user_input('Your choice: ')
-            if user_choice.lower() == 'a':
+            for key, value in SEARCH_MENU.items():
+                print('{}) {}'.format(key, value))
+            user_choice = self.get_user_input('Your choice: ').lower().strip()
+            if user_choice == 'a':
                 date = self.get_date_input()
                 SearchUtil.search_tasks(
                     date, self.tasks, SearchUtil.search_exact_date)
-            elif user_choice.lower() == 'b':
+            elif user_choice == 'b':
                 time = self.get_user_input('Time in minutes: ')
                 SearchUtil.search_tasks(
                     time, self.tasks, SearchUtil.search_by_minutes)
-            elif user_choice.lower() == 'c':
+            elif user_choice == 'c':
                 user_query = self.get_user_input('Enter your search query: ')
                 SearchUtil.search_tasks(
                     user_query, self.tasks, SearchUtil.exact_search)
-            elif user_choice.lower() == 'd':
-                print('you chose d')
-            elif user_choice.lower() == 'e':
+            elif user_choice == 'd':
                 pattern = self.get_user_input('Enter your regex pattern: ')
                 SearchUtil.search_tasks(pattern, self.tasks, SearchUtil.regex_search)
-            elif user_choice.lower() == 'f':
-                waiting_for_input = False
+            elif user_choice == 'e':
+                break
             else:
-                print('Command not recognized, please try again')
+                print('Option not recognized, please try again')
 
     def gather_task_data(self):
         task_date = self.get_date_input()
-        task_title = get_user_input('Title of the task: ')
-        task_time = get_user_input('Time spent (rounded in minutes): ')
-        task_notes = get_user_input(
+        task_title = self.get_user_input('Task title: ')
+        task_time = self.get_time_input()
+        task_notes = self.get_user_input(
             'Notes (Optional, you can leave this empty): ')
         task = {
             'date': task_date,
@@ -68,20 +76,33 @@ class WorkLog:
         LogUtil.save_to_log(task)
         self.tasks.append(OrderedDict(
             [('date', task_date), ('title', task_title), ('time', task_time), ('notes', task_notes)]))
-        input('The entry has been added. Press enter to return to the menu ')
+        input('The entry has been added! Press enter to return to the menu ')
+
+
+    def get_time_input(self):
+        waiting_for_valid_time = True
+        while waiting_for_valid_time:
+            minutes = self.get_user_input('Time spent (rounded in minutes): ').strip()
+            try:
+                time = int(minutes)
+                waiting_for_valid_time = False
+                return time
+            except ValueError:
+                input('Please enter numbers only! Press enter to try again ')
+
 
     def get_date_input(self):
         waiting_for_valid_date = True
         while waiting_for_valid_date:
             print('Date of the task')
-            date_input = input('Please use DD/MM/YYYY: ')
+            date_input = input('Please use DD/MM/YYYY: ').strip()
             date_list = [int(date) for date in date_input.split('/')]
             try:
                 task_date = datetime.date(
                     date_list[2], date_list[1], date_list[0])
                 waiting_for_valid_date = False
                 return date_input
-            except ValueError:
+            except (ValueError, IndexError):
                 print('Error: {} doesn\'t seem to be a valid date'.format(date_input))
                 input('Press enter to try again ')
 
@@ -89,24 +110,24 @@ class WorkLog:
         return input(message)
 
     def print_menu(self):
+        print('='*15)
         print('WORK LOG')
+        print('='*15)
         print('What would you like to do?')
-        print('a) Add new entry')
-        print('b) Search existing entries')
-        print('c) Quit the program')
+        for key, value in MAIN_MENU.items():
+            print('{}) {}'.format(key, value))
 
     def prompt_user(self):
         LogUtil.check_for_log()
-        waiting_for_input = True
-        while waiting_for_input:
+        while True:
             self.print_menu()
-            user_choice = self.get_user_input('Your choice: ')
-            if user_choice.lower() == 'a':
+            user_choice = self.get_user_input('Your choice: ').lower().strip()
+            if user_choice == 'a':
                 self.gather_task_data()
-            elif user_choice.lower() == 'b':
+            elif user_choice == 'b':
                 self.search_menu()
-            elif user_choice.lower() == 'c':
-                waiting_for_input = False
+            elif user_choice == 'c':
+                break
             else:
                 print('Option not recognized, please try again.')
 
